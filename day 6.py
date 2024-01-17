30ee1b2fe01d95a7cfbd9a3ca134740fbdd3de4b
HEPHZIBAI <113971953+HEPHZIBAI@users.noreply.github.com>
2024-01-17 17:46:00 +0530

    Add files via upload

---
+++ day 6.py
@@ -0,0 +1,6 @@
+'''
+Write a program to count the occurrences of a specific character in a string.
+'''
+a=input().strip()
+b=input()
+print(a.count(b))
---
+++ day 7.py
@@ -0,0 +1,11 @@
+'''
+Write a program to check if a number is positive, negative, or zero.
+'''
+
+a=int(input())
+if(a==0):
+	print("zero")
+elif(a>0):
+	print("positive")
+else:
+	print("negative")

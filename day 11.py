30af00a226a464986e71b4331e169a1b72e3b95f
HEPHZIBAI <113971953+HEPHZIBAI@users.noreply.github.com>
2024-01-22 09:26:16 +0530

    Add files via upload

---
+++ day 11.py
@@ -0,0 +1,7 @@
+'''
+Write a program to print the multiplication table of a given number.
+'''
+
+a=int(input())
+for i in range(1,11):
+	print(i,' X ',a,' = ',i*a)
---
+++ day 12.py
@@ -0,0 +1,6 @@
+'''
+Write a program to reverse a given string.
+'''
+
+a=input().strip()
+print(a[::-1)
